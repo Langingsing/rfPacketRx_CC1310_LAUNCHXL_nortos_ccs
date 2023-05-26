3aade8c8a361742b61b438ef3c6ab47a741938b6
ZhaoPeichen <1981336562@qq.com>
2023-05-26 22:56:39 +0800

    reformat code

--- rfPacketRx.c
+++ rfPacketRx.c
@@ -39,7 +39,9 @@
 #include <ti/drivers/PIN.h>
 
 /* Driverlib Header files */
+// @formatter:off
 #include DeviceFamily_constructPath(driverlib/rf_prop_mailbox.h)
+// @formatter:on
 
 /* Board Header files */
 #include "Board.h"
@@ -58,7 +60,6 @@
                                    * 1 Header byte (RF_cmdPropRx.rxConf.bIncludeHdr = 0x1)
                                    * Max 30 payload bytes
                                    * 1 status byte (RF_cmdPropRx.rxConf.bAppendStatus = 0x1) */
-
 
 
 /***** Prototypes *****/
@@ -88,19 +89,19 @@ rxDataEntryBuffer[RF_QUEUE_DATA_ENTRY_BUFFER_SIZE(NUM_DATA_ENTRIES,
                                                   NUM_APPENDED_BYTES)];
 #elif defined(__GNUC__)
 static uint8_t
-rxDataEntryBuffer[RF_QUEUE_DATA_ENTRY_BUFFER_SIZE(NUM_DATA_ENTRIES,
-                                                  MAX_LENGTH,
-                                                  NUM_APPENDED_BYTES)]
-                                                  __attribute__((aligned(4)));
+        rxDataEntryBuffer[RF_QUEUE_DATA_ENTRY_BUFFER_SIZE(NUM_DATA_ENTRIES,
+                                                          MAX_LENGTH,
+                                                          NUM_APPENDED_BYTES)]
+        __attribute__((aligned(4)));
 #else
 #error This compiler is not supported.
 #endif
 
 /* Receive dataQueue for RF Core to fill in data */
 static dataQueue_t dataQueue;
-static rfc_dataEntryGeneral_t* currentDataEntry;
+static rfc_dataEntryGeneral_t *currentDataEntry;
 static uint8_t packetLength;
-static uint8_t* packetDataPointer;
+static uint8_t *packetDataPointer;
 
 
 static uint8_t packet[MAX_LENGTH + NUM_APPENDED_BYTES - 1]; /* The length byte is stored in a separate variable */
@@ -109,34 +110,30 @@ static uint8_t packet[MAX_LENGTH + NUM_APPENDED_BYTES - 1]; /* The length byte i
  * Application LED pin configuration table:
  *   - All LEDs board LEDs are off.
  */
-PIN_Config pinTable[] =
-{
-    Board_PIN_LED2 | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,
-	PIN_TERMINATE
+PIN_Config pinTable[] = {
+        Board_PIN_LED2 | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,
+        PIN_TERMINATE
 };
 
 /***** Function definitions *****/
 
-void *mainThread(void *arg0)
-{
+void *mainThread(void *arg0) {
     RF_Params rfParams;
     RF_Params_init(&rfParams);
 
     /* Open LED pins */
     ledPinHandle = PIN_open(&ledPinState, pinTable);
-    if (ledPinHandle == NULL)
-    {
-        while(1);
+    if (ledPinHandle == NULL) {
+        while (1);
     }
 
-    if( RFQueue_defineQueue(&dataQueue,
+    if (RFQueue_defineQueue(&dataQueue,
                             rxDataEntryBuffer,
                             sizeof(rxDataEntryBuffer),
                             NUM_DATA_ENTRIES,
-                            MAX_LENGTH + NUM_APPENDED_BYTES))
-    {
+                            MAX_LENGTH + NUM_APPENDED_BYTES)) {
         /* Failed to allocate space for all data entries */
-        while(1);
+        while (1);
     }
 
     /* Modify CMD_PROP_RX command for application needs */
@@ -155,19 +152,18 @@ void *mainThread(void *arg0)
 #if defined(DeviceFamily_CC26X0R2)
     rfHandle = RF_open(&rfObject, &RF_prop, (RF_RadioSetup*)&RF_cmdPropRadioSetup, &rfParams);
 #else
-    rfHandle = RF_open(&rfObject, &RF_prop, (RF_RadioSetup*)&RF_cmdPropRadioDivSetup, &rfParams);
+    rfHandle = RF_open(&rfObject, &RF_prop, (RF_RadioSetup *) &RF_cmdPropRadioDivSetup, &rfParams);
 #endif// DeviceFamily_CC26X0R2
 
     /* Set the frequency */
-    RF_postCmd(rfHandle, (RF_Op*)&RF_cmdFs, RF_PriorityNormal, NULL, 0);
+    RF_postCmd(rfHandle, (RF_Op *) &RF_cmdFs, RF_PriorityNormal, NULL, 0);
 
     /* Enter RX mode and stay forever in RX */
-    RF_EventMask terminationReason = RF_runCmd(rfHandle, (RF_Op*)&RF_cmdPropRx,
+    RF_EventMask terminationReason = RF_runCmd(rfHandle, (RF_Op *) &RF_cmdPropRx,
                                                RF_PriorityNormal, &callback,
                                                RF_EventRxEntryDone);
 
-    switch(terminationReason)
-    {
+    switch (terminationReason) {
         case RF_EventLastCmdDone:
             // A stand-alone radio operation command or the last radio
             // operation command in a chain finished.
@@ -186,12 +182,11 @@ void *mainThread(void *arg0)
             break;
         default:
             // Uncaught error event
-            while(1);
+            while (1);
     }
 
-    uint32_t cmdStatus = ((volatile RF_Op*)&RF_cmdPropRx)->status;
-    switch(cmdStatus)
-    {
+    uint32_t cmdStatus = ((volatile RF_Op *) &RF_cmdPropRx)->status;
+    switch (cmdStatus) {
         case PROP_DONE_OK:
             // Packet received with CRC OK
             break;
@@ -240,16 +235,14 @@ void *mainThread(void *arg0)
         default:
             // Uncaught error event - these could come from the
             // pool of states defined in rf_mailbox.h
-            while(1);
+            while (1);
     }
 
-    while(1);
+    while (1);
 }
 
-void callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
-{
-    if (e & RF_EventRxEntryDone)
-    {
+void callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e) {
+    if (e & RF_EventRxEntryDone) {
         /* Toggle pin to indicate RX */
         PIN_setOutputValue(ledPinHandle, Board_PIN_LED2,
                            !PIN_getOutputValue(Board_PIN_LED2));
@@ -260,8 +253,8 @@ void callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
         /* Handle the packet data, located at &currentDataEntry->data:
          * - Length is the first byte with the current configuration
          * - Data starts from the second byte */
-        packetLength      = *(uint8_t*)(&currentDataEntry->data);
-        packetDataPointer = (uint8_t*)(&currentDataEntry->data + 1);
+        packetLength = *(uint8_t *) (&currentDataEntry->data);
+        packetDataPointer = (uint8_t *) (&currentDataEntry->data + 1);
 
         /* Copy the payload + the status byte to the packet variable */
         memcpy(packet, packetDataPointer, (packetLength + 1));
